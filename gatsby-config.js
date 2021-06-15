c367a9eb7347e076c9b7d7844a11cdef6e9adc2e
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2021-06-14 23:03:35 -0400

    remove more plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,14 +6,14 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
+    // `gatsby-plugin-image`,
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `images`,
+    //     path: `${__dirname}/src/images`,
+    //   },
+    // },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     // {
